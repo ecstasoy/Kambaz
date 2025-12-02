94436b69f09e89e8aad05a59792251c66df5eec7
Kunhua Huang <huang.kunh@northeastern.edu>
2025-12-01 21:47:56 -0500

    lab6

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -2,11 +2,10 @@
 
 import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
-import { Table } from "react-bootstrap";
-import { FaUserCircle } from "react-icons/fa";
+import PeopleTable from "../Table";
 import * as client from "../client";
 
-export default function PeopleTable() {
+export default function PeopleTablePage() {
     const { cid } = useParams();
     const [users, setUsers] = useState<any[]>([]);
     
@@ -19,36 +18,5 @@ export default function PeopleTable() {
         fetchUsers();
     }, []);
     
-    return (
-        <div id="wd-people-table">
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Login ID</th>
-                        <th>Section</th>
-                        <th>Role</th>
-                        <th>Last Activity</th>
-                        <th>Total Activity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user: any) => (
-                            <tr key={user._id}>
-                                <td className="wd-full-name text-nowrap">
-                                    <FaUserCircle className="me-2 fs-1 text-secondary" />
-                                    <span className="wd-first-name">{user.firstName}</span>{" "}
-                                    <span className="wd-last-name">{user.lastName}</span>
-                                </td>
-                                <td className="wd-login-id">{user.loginId}</td>
-                                <td className="wd-section">{user.section}</td>
-                                <td className="wd-role">{user.role}</td>
-                                <td className="wd-last-activity">{user.lastActivity}</td>
-                                <td className="wd-total-activity">{user.totalActivity}</td>
-                            </tr>
-                        ))}
-                </tbody>
-            </Table>
-        </div>
-    );
+    return <PeopleTable users={users} fetchUsers={fetchUsers} />;
 }
